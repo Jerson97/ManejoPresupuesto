272e737f6776af60b4d3dcd2e160568e81b979e4
Jerson97 <jersonramirezs97@gmail.com>
2025-08-20 16:04:31 -0500

    Feat: Se agrego la funcion para ver los reporte por mes

--- ManejoPresupuesto/Repositories/Implementation/RepositorioTransacciones.cs
+++ ManejoPresupuesto/Repositories/Implementation/RepositorioTransacciones.cs
@@ -112,6 +112,20 @@ namespace ManejoPresupuesto.Repositories.Implementation
                     GROUP BY datediff(d, @fechaInicio, FechaTransaccion) / 7, cat.TipoOperacionId
                     ", modelo);
         }
+
+        public async Task<IEnumerable<ResultadoObtenerPorMes>> ObtenerPorMes(int usuarioId, int año)
+        {
+            using var connection = new SqlConnection(connectionString);
+            return await connection.QueryAsync<ResultadoObtenerPorMes>(@"
+            SELECT MONTH(FechaTransaccion) as Mes,
+            SUM(Monto) as Monto, cat.TipoOperacionId
+            FROM Transacciones
+            INNER JOIN Categorias cat
+            ON cat.Id = Transacciones.CategoriaId
+            WHERE Transacciones.UsuarioId = @usuarioId AND YEAR(FechaTransaccion) = @Año
+            GROUP BY Month(FechaTransaccion), cat.TipoOperacionId
+            ", new {usuarioId, año});
+        }
         public async Task Borrar(int id)
         {
             using var connection = new SqlConnection(connectionString);
